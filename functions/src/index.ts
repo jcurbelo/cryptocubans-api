72eb77d507ae5a3c1ad0f48a6def8a6c67b25661
jcurbelo <curbelorobin@gmail.com>
2022-02-04 17:21:28 -0500

    Adds videos support

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,12 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 import {User} from "./user.interface";
+import {ethers} from "ethers";
+import axios from "axios";
 
 admin.initializeApp();
 
 export const auth = functions.https.onRequest(async (req, res) => {
-  functions.logger.info("Auth trigger", {structuredData: true});
   const db = admin.firestore();
   const {address} = req.query;
 
@@ -27,3 +28,63 @@ export const auth = functions.https.onRequest(async (req, res) => {
 
   res.status(200).json(user);
 });
+
+
+export const verify = functions.https.onRequest(async (req, res) => {
+  const db = admin.firestore();
+  const {address, signature} = req.query;
+
+  const data = await db.collection("users")
+      .where("address", "==", address)
+      .get();
+
+  if (data.empty) {
+    res.status(400).json({error: "User not found"});
+    return;
+  }
+
+  const user = data.docs[0].data() as User;
+
+  const decodedAddress = ethers.utils
+      .verifyMessage(user.nonce?.toString() || "", signature as string);
+
+  if (decodedAddress !== address) {
+    res.status(400).json({error: "Invalid signature"});
+    return;
+  }
+
+  const tokenIds = await _getTokens(address);
+
+  const videosData = await db.collection("videos")
+      .where("tokenId", "in", tokenIds)
+      .get();
+
+  if (!videosData.empty) {
+    const links = videosData.docs.map((doc) => doc.data()["link"] as string);
+    res.status(200).json(links);
+  }
+
+  res.status(200).json([]);
+});
+
+// eslint-disable-next-line require-jsdoc
+async function _getTokens(address: string): Promise<number[]> {
+  let results: number[] = [];
+  const contractAddress = "0xca9eE3460D84Eac6C2F2284CFe3E3B35A2267d78";
+  const resp = await axios.get(
+      `https://api.opensea.io/api/v1/assets?owner=${address}&asset_contract_address=${contractAddress}`,
+      {
+        headers: {
+          "x-api-key": "847dbb3dc2ec45c895795c69015f6fc3",
+        },
+      }
+  );
+
+  const {assets} = resp.data;
+
+  if (assets && assets.length > 0) {
+    results = assets.map((asset: any) => Number(asset["token_id"]));
+  }
+
+  return results;
+}
