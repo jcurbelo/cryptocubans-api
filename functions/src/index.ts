4b9009364dd5e76bed621316a3c7456a9e7d9e0c
jcurbelo <curbelorobin@gmail.com>
2022-02-04 16:44:57 -0500

    Fixed update user changes

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -13,15 +13,17 @@ export const auth = functions.https.onRequest(async (req, res) => {
       .where("address", "==", address)
       .get();
 
-  let user: User = {id: "", address: address as string};
+  let user: User = {address: address as string};
 
   if (data.empty) {
     user = await db.collection("users").add(user);
+  } else {
+    user.id = data.docs[0].id;
   }
   user.nonce = Math.floor(Math.random() * 10000000);
   await db.collection("users")
-      .doc(user.id)
-      .update({nonce: user.nonce, address: user.address});
+      .doc(user.id as string)
+      .update({nonce: user.nonce});
 
   res.status(200).json(user);
 });
--- functions/src/user.interface.ts
+++ functions/src/user.interface.ts
@@ -1,5 +1,5 @@
 export interface User {
-    id: string;
+    id?: string;
     address?: string;// wallet address
     nonce?: number;
     tokenIds?: number[];
